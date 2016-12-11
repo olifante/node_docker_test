dfec1d04543e15e234a2c9cd46b348c630128177
Tiago Henriques <trinosauro@gmail.com>
2016-12-11 15:55:03 +0100

    Add start script

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-Load the http module to create an http server.
+// Load the http module to create an http server.
 var http = require('http');
 
 // Configure our HTTP server to respond with Hello World to all requests.
@@ -8,7 +8,7 @@ response.end("Hello World\n");
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+server.listen(8000, "0.0.0.0");
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
